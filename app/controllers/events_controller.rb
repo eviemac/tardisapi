022e3eb0f0639e13cfdec7b9088c051013eec31b
E W Macharia <macharia@doctors.org.uk>
2012-11-27 08:59:13 +0000

    new model events. views need trouble shooting

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,60 +25,56 @@ class EventsController < ApplicationController
     end
     # GET /users/:user_id/events/new
     # GET /users/:user_id/events/new.xml
-    def new
-      #1st you retrieve the user thanks to params[:user_id]
-      user = User.find(params[:user_id])
-      #2nd you build a new one
-      @event = user.events.build
-      respond_to do |format|
-        format.html # new.html.erb
-        format.xml  { render :xml => @event }
-      end
-    end
+def new
+  @user = User.find(params[:user_id])
+  @event = @user.events.build
+end
     # GET /users/:user_id/events/:id/edit
-    def edit
+def edit
       #1st you retrieve the user thanks to params[:user_id]
-      user = User.find(params[:user_id])
+  user = User.find(params[:user_id])
       #2nd you retrieve the event thanks to params[:id]
-      @event = user.events.find(params[:id])
-    end
+  @event = user.events.find(params[:id])
+end
     # POST /users/:user_id/events
     # POST /users/:user_id/events.xml
-    def create
+def create
       #1st you retrieve the user thanks to params[:user_id]
-      user = User.find(params[:user_id])
+  user = User.find(params[:user_id])
       #2nd you create the event with arguments in params[:event]
-      @event = user.events.create(params[:event])
-      respond_to do |format|
-        if @event.save
+  @event = user.events.create(params[:event])
+  respond_to do |format|
+    if @event.save
           #1st argument of redirect_to is an array, in order to build the correct route to the nested resource event
-          format.html { redirect_to([@event.user, @event], :notice => 'Comment was successfully created.') }
+      format.html { redirect_to([@event.user, @event], :notice => 'Comment was successfully created.') }
           #the key :location is associated to an array in order to build the correct route to the nested resource event
-          format.xml  { render :xml => @event, :status => :created, :location => [@event.user, @event] }
-        else
-          format.html { render :action => "new" }
-          format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
-        end
-      end
+      format.xml  { render :xml => @event, :status => :created, :location => [@event.user, @event] }
+    else
+      format.html { render :action => "new" }
+      format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
     end
-    # PUT /users/:user_id/events/:id
-    # PUT /users/:user_id/events/:id.xml
-    def update
+  end
+end
+
+
+# PUT /users/:user_id/events/:id
+# PUT /users/:user_id/events/:id.xml
+def update
       #1st you retrieve the user thanks to params[:user_id]
-      user = User.find(params[:user_id])
+  user = User.find(params[:user_id])
       #2nd you retrieve the event thanks to params[:id]
-      @event = user.events.find(params[:id])
-      respond_to do |format|
-        if @event.update_attributes(params[:event])
+  @event = user.events.find(params[:id])
+  respond_to do |format|
+    if @event.update_attributes(params[:event])
           #1st argument of redirect_to is an array, in order to build the correct route to the nested resource event
-          format.html { redirect_to([@event.user, @event], :notice => 'Comment was successfully updated.') }
-          format.xml  { head :ok }
-        else
-          format.html { render :action => "edit" }
-          format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
-        end
-      end
+      format.html { redirect_to([@event.user, @event], :notice => 'Comment was successfully updated.') }
+      format.xml  { head :ok }
+    else
+      format.html { render :action => "edit" }
+      format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
     end
+  end
+end
     # DELETE /users/:user_id/events/1
     # DELETE /users/:user_id/events/1.xml
     def destroy
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
+  validates :eventname, :presence => true
+  validates :eventtime, :presence => true
+
   belongs_to :user
-  attr_accessible :eventname, :eventtime
+
+  #attr_accessible :eventname, :eventtime
 end
